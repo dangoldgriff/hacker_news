07aaa8802c546e2271e8ecb7a9f0b0ca729ecfa7
Dan <dangoldgriff@gmail.com>
2013-11-15 16:35:46 -0800

    update vote create to have current_user conditional

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,13 @@
 class VotesController < ApplicationController
   def create
-    @vote = current_user.votes.create(vote_params)
-    redirect_to "/links", notice: "Your vote has been counted!"
+    if @current_user = nil
+      @vote = current_user.votes.create(vote_params)
+      redirect_to "/links", notice: "Your vote has been counted!"
+    else
+      redirect_to "/users/new", notice: "You must sign in to vote"
+    end
   end
+
 
 private
 
